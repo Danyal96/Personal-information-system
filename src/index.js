f211232abba5c70a886b1f4b40c08e5580d617e4
danyal amirhosseini <danyalamirhosseini1381@gmail.com>
2024-04-21 23:43:29 +0330

    add fake api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./index.css"
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.rtl.min.css"
+import { ThemeProvider } from 'react-bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ThemeProvider dir='rtl'>
+        <App />
+    </ThemeProvider>
 );
 
